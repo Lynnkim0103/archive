87c3f7478230ab1669fc7abb3e26646e3bba34c8
김예슬 <126546284+Lynnkim0103@users.noreply.github.com>
2023-04-03 09:31:48 +0900

    Add files via upload

---
+++ 수업실습/2023-04-01/script.js
@@ -0,0 +1,69 @@
+let todos = [];
+
+
+
+const save = () => {
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
+const delItem = (event) => {
+  // console.log('삭제!');
+  // console.log(event.target.parentElement);
+  const target = event.target.parentElement;
+  todos = todos.filter((todo) => todo.id != target.id);
+  save();
+  target.remove();
+}
+
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const ul = document.querySelector('ul');
+
+const addItem = (todo) => {
+    if(todo.text !== '') {
+    const li = document.createElement('li');
+    const button = document.createElement('button');
+    const span = document.createElement('span');
+
+    span.innerText = todo.text
+    button.innerText = '삭제';
+    button.addEventListener('click', delItem);
+
+    li.appendChild(span);
+    li.appendChild(button);
+    ul.appendChild(li);
+    li.id = todo.id;
+  }
+}
+const handler = (event) => {
+  event.preventDefault();
+
+  const todo = {
+    id: Date.now(),
+    text: input.value
+  };
+
+  todos.push(todo);
+  addItem(todo);
+  save();
+  input.value = '';
+}
+
+const init = () => {
+  // const userTodos = localStorage.getItem('todos');
+  // console.log(userTodos);
+  const userTodos = JSON.parse(localStorage.getItem('todos'));
+  if(userTodos) {
+    userTodos.forEach((todo) => {
+      addItem(todo);
+    });
+  }
+}
+
+init();
+form.addEventListener('submit', handler);
+
+/*localStorage.setItem('Hello', 'world');
+const myData = localStorage.getItem('Hello');
+console.log(myData);*/
+
+
